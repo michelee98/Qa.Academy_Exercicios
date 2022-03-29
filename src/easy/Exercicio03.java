0abf44eba0a83463a27b1b1b9520d6de97d595ff
Michele Sousa <michelee98.ss@gmail.com>
2022-03-28 23:55:24 -0300

    exercicio3

--- src/easy/Exercicio03.java
+++ src/easy/Exercicio03.java
@@ -4,14 +4,13 @@ import javax.swing.JOptionPane;
 
 public class Exercicio03 {
 
-    public static void main(String[] args) {
+    public void retornaNumeroDigitado(){
         String numeroDigitado1;
         String numeroDigitado2;
         numeroDigitado1 = JOptionPane.showInputDialog("numeroDigitado1");
         numeroDigitado2 = JOptionPane.showInputDialog("numeroDigitado2");
         System.out.println("o valor do primeiro numero é:" + numeroDigitado1);
         System.out.println("o valor do primeiro numero é:" + numeroDigitado2);
-
     }
 
 }
